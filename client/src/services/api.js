ff50f1ae569558f955e23ced49da5cca87ad852d
Vanshul <vanshuldahiya2002@gmail.com>
2023-04-17 17:03:28 +0530

    backend deployed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://file-sharing-backend-ljuk.onrender.com";
 
 export const uploadFile = async (data) => {
   try {
